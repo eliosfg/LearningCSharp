4f8ffaa1cabf83b93ca8a3713994b6ac60f60502
Saul.Fuentes <eliosfg@gmail.com>
2025-01-21 21:40:47 -0400

    Added files from C# course

---
+++ Start/Overview/Comments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main simple application function
+        /// </summary>
+        /// <param name="args">An array of string arguments from the command line</param>
+        /// <returns>
+        /// No return value
+        /// </returns>
+        static void Main(string[] args)
+        {
+            // Single-line comments start with two slashes
+            // You can have as many as you want
+            Console.WriteLine("Hello World!");
+            /* Multi-line comments start with a slash and asterisk
+             * They can span multiple lines
+             * And end with an asterisk and slash */
+        }
+    }
+}
---
+++ Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine() ?? "Guest";
+            Console.WriteLine("Hello there " + name + "!");
+        }
+    }
+}
